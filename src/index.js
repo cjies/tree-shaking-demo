136895443ae972c968fbe5d79d214e7374dd7ecb
CJ <cijies@gmail.com>
2017-03-26 23:09:01 +0800

    Disable import lodash example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { sayHAHAHA, sayTEEHEE } from './say_something';
-import { rounding } from './math';
 
 console.log(sayHAHAHA);
 console.log(sayTEEHEE);
-console.log(rounding(4.006, 2));
+
+// -------------------------------------
+//   import lodash helpers
+// -------------------------------------
+
+// import { rounding } from './math';
+
+// console.log(rounding(4.006, 2));
